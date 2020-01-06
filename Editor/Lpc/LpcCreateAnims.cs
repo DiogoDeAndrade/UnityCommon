b931fad8328eb5217a990b1080d0240a7f035ed7
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-01-06 20:42:12 +0000

    Added LPC processing utilities

---
+++ Editor/Lpc/LpcCreateAnims.cs
@@ -0,0 +1,219 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class LpcCreateAnims
+{
+    struct AnimKey
+    {
+        public LpcSpriteProcessor.LpcAnimationState state;
+        public char                                 direction;
+    };
+
+    public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, AnimationClip>>
+    {
+        public AnimationClipOverrides(int capacity) : base(capacity) { }
+
+        public AnimationClip this[string name]
+        {
+            get
+            {
+                return this.Find(x => x.Key.name.Equals(name)).Value;
+            }
+            set
+            {
+                int index = this.FindIndex(x => x.Key.name.Equals(name));
+                if (index != -1)
+                {
+                    this[index] = new KeyValuePair<AnimationClip, AnimationClip>(this[index].Key, value);
+                }
+            }
+        }
+    }
+
+    [MenuItem("Assets/Create/LPC/Create Animations")]
+    private static void LPCCreateAnimations()
+    {
+        // Get all sprites
+        Texture2D texture = Selection.activeObject as Texture2D;
+
+        string assetPath = AssetDatabase.GetAssetPath(texture);
+
+        var      assetsAtPath = AssetDatabase.LoadAllAssetsAtPath(assetPath);
+        var      sprites = new List<Sprite>();
+
+        foreach (var asset in assetsAtPath)
+        {
+            if (asset.GetType() == typeof(Sprite))
+            {
+                sprites.Add(asset as Sprite);
+            }
+        }
+
+        if (sprites.Count > 0)
+        {
+            Dictionary<AnimKey, List<Sprite>> anims = new Dictionary<AnimKey, List<Sprite>>();
+
+            for (int i = 0; i < sprites.Count; i++)
+            {
+                Rect rect = sprites[i].rect;
+                int row = (int)(rect.y / 64);
+                var animState = LpcSpriteProcessor.GetAnimationState(row);
+
+                AnimKey ak = new AnimKey();
+                ak.state = animState;
+                if (sprites[i].name.IndexOf("N_") != -1) ak.direction = 'N';
+                else if (sprites[i].name.IndexOf("E_") != -1) ak.direction = 'E';
+                else if (sprites[i].name.IndexOf("S_") != -1) ak.direction = 'S';
+                else if (sprites[i].name.IndexOf("W_") != -1) ak.direction = 'W';
+
+                if (!anims.ContainsKey(ak)) anims.Add(ak, new List<Sprite>());
+
+                if (anims[ak] == null) anims[ak] = new List<Sprite>();
+                anims[ak].Add(sprites[i]);
+            }
+
+            int     framesPerSecond = 12;
+            string  filename = "";
+            string  path = Path.GetDirectoryName(assetPath);
+
+            // Create animations
+            foreach (var anim in anims)
+            {
+                bool    loop = false;
+
+                if (anim.Key.state == LpcSpriteProcessor.LpcAnimationState.Walk)
+                {
+                    loop = true;
+
+                    // Build "Idle" animation from this as well
+                    var idleClip = CreateAnimationClip("Idle" + anim.Key.direction, framesPerSecond, loop, new List<Sprite>(anim.Value.Take(1)));
+
+                    filename = path + "/" + idleClip.name + ".anim";
+                    AssetUtils.CreateOrReplaceAsset<AnimationClip>(idleClip, filename);
+
+                    var clip = CreateAnimationClip(anim.Key.state.ToString() + anim.Key.direction, framesPerSecond, loop, new List<Sprite>(anim.Value.Skip(1).Take(8)));
+
+                    filename = path + "/" + clip.name + ".anim";
+                    AssetUtils.CreateOrReplaceAsset<AnimationClip>(clip, filename);
+                }
+                else if (anim.Key.state == LpcSpriteProcessor.LpcAnimationState.Shoot)
+                {
+                    // Split frames in two
+                    var shootBegin = CreateAnimationClip("ShootBegin" + anim.Key.direction, framesPerSecond, false, new List<Sprite>(anim.Value.Take(5)));
+
+                    filename = path + "/" + shootBegin.name + ".anim";
+                    AssetUtils.CreateOrReplaceAsset<AnimationClip>(shootBegin, filename);
+
+                    var clip = CreateAnimationClip("Shoot" + anim.Key.direction, framesPerSecond, true, new List<Sprite>(anim.Value.Skip(5).Take(7)));
+
+                    filename = path + "/" + clip.name + ".anim";
+                    AssetUtils.CreateOrReplaceAsset<AnimationClip>(clip, filename);
+                }
+                else
+                {
+                    var clip = CreateAnimationClip(anim.Key.state.ToString() + anim.Key.direction, framesPerSecond, loop, anim.Value);
+
+                    filename = path + "/" + clip.name + ".anim";
+                    AssetUtils.CreateOrReplaceAsset<AnimationClip>(clip, filename);
+                }
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            var runtimeController = AssetDatabase.LoadAssetAtPath<RuntimeAnimatorController>("Assets/Animation/Base/BaseAnimations.controller");
+            AnimatorOverrideController controller = new AnimatorOverrideController(runtimeController);
+
+            var overrides = new AnimationClipOverrides(controller.overridesCount);
+            controller.GetOverrides(overrides);
+
+            List<char> dirs = new List<char> { 'N', 'E', 'S', 'W' };
+
+            foreach (var c in dirs)
+            {
+                AnimationClip anim;
+
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Hurt" + c + ".anim");
+                if (anim != null) overrides["Die" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Idle" + c + ".anim");
+                if (anim != null) overrides["Idle" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/ShootBegin" + c + ".anim");
+                if (anim != null) overrides["ShootBegin" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Shoot" + c + ".anim");
+                if (anim != null) overrides["Shoot" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Slash" + c + ".anim");
+                if (anim != null) overrides["Slash" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Thrust" + c + ".anim");
+                if (anim != null) overrides["Thrust" + c] = anim;
+                anim = AssetDatabase.LoadAssetAtPath<AnimationClip>(path + "/Walk" + c + ".anim");
+                if (anim != null) overrides["Walk" + c] = anim;
+            }
+
+            controller.ApplyOverrides(overrides);
+
+            filename = path + "/" + Path.GetFileNameWithoutExtension(assetPath) + ".controller";
+            AssetUtils.CreateOrReplaceAsset<AnimatorOverrideController>(controller, filename);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+
+    static AnimationClip CreateAnimationClip(string name, int framesPerSecond, bool loop, List<Sprite> sprites)
+    {
+        float timePerFrame = 1.0f / framesPerSecond;
+
+        AnimationClip clip = new AnimationClip();
+        clip.name = name;
+        clip.frameRate = framesPerSecond;
+
+        var settings = AnimationUtility.GetAnimationClipSettings(clip);
+        settings.loopTime = loop;
+        AnimationUtility.SetAnimationClipSettings(clip, settings);
+
+        EditorCurveBinding spriteBinding = new EditorCurveBinding();
+        spriteBinding.type = typeof(SpriteRenderer);
+        spriteBinding.path = "";
+        spriteBinding.propertyName = "m_Sprite";
+
+        ObjectReferenceKeyframe[] spriteKeyFrames = new ObjectReferenceKeyframe[sprites.Count];
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            spriteKeyFrames[i] = new ObjectReferenceKeyframe();
+            spriteKeyFrames[i].time = i * timePerFrame;
+            spriteKeyFrames[i].value = sprites[i];
+        }
+
+        AnimationUtility.SetObjectReferenceCurve(clip, spriteBinding, spriteKeyFrames);
+
+        return clip;
+    }
+
+    [MenuItem("Assets/Create/LPC/Create Animations", validate = true)]
+    private static bool NewMenuOptionValidation()
+    {
+        var selectedObject = Selection.activeObject;
+
+        if (selectedObject.GetType() != typeof(Texture2D))
+        {
+            Debug.LogWarning("Can only create LPC animations from texture!");
+            return false;
+        }
+
+        Texture2D texture = selectedObject as Texture2D;
+
+        if (texture.name.Length < 4) return false;
+
+        if (texture.name.Substring(0,4) != "LPC_")
+        {
+            Debug.LogWarning("Texture name needs to start with LPC_!");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ Editor/Lpc/LpcSpriteProcessor.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+public class LpcSpriteProcessor : AssetPostprocessor {
+
+	public enum LpcAnimationState
+	{
+		Hurt,
+		Shoot,
+		Slash,
+		Walk,
+		Thrust,
+		Spellcast
+	}
+
+	private const int LPC_SHEET_WIDTH  = 832;
+	private const int LPC_SHEET_HEIGHT = 1344;
+	private const int LPC_SPRITE_SIZE  = 64;
+
+	private int m_PixelsPerUnit; // Sets the Pixels Per Unit in the Importer
+	private int m_ScFrames;      // Spellcast animation frames
+	private int m_ThFrames;      // Thrust animation frames
+	private int m_WcFrames;      // Walkcycle animation frames
+	private int m_SlFrames;      // Slash animation frames
+	private int m_ShFrames;      // Shoot animation frames
+	private int m_HuFrames;      // Hurt animation frames
+
+	private bool m_ImportEmptySprites;
+	private int m_ColCount;
+	private int m_RowCount;
+
+	void RetrieveSettings(){
+		// Retrieve Basic Settings
+		m_ImportEmptySprites = LpcSpriteSettings.GetImportEmptySprites();
+		m_PixelsPerUnit = LpcSpriteSettings.GetPixelsPerUnit ();
+
+		// Retrieve Animation Settings
+		m_ScFrames = LpcSpriteSettings.GetScFrameCount();
+		m_ThFrames = LpcSpriteSettings.GetThFrameCount();
+		m_WcFrames = LpcSpriteSettings.GetWcFrameCount();
+		m_SlFrames = LpcSpriteSettings.GetSlFrameCount();
+		m_ShFrames = LpcSpriteSettings.GetShFrameCount();
+		m_HuFrames = LpcSpriteSettings.GetHuFrameCount();
+
+		// Retrieve Other Settings
+		m_ColCount = LpcSpriteSettings.GetColCount();
+		m_RowCount = LpcSpriteSettings.GetRowCount();
+	}
+
+	void OnPreprocessTexture()
+	{
+        string filename = Path.GetFileName(assetPath);
+        if (filename.Length > 4)
+        {
+            if (filename.Substring(0, 4) != "LPC_") return;
+        }
+
+        // Can't load texture yet, asset does not exist at this stage
+		/*Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
+        if (texture == null)
+        {
+            var assetType = AssetDatabase.GetMainAssetTypeAtPath(assetPath);
+            Debug.LogError("Not a texture (" + assetType + ") [" + assetPath + "]");
+            return;
+        }
+        if (!IsLpcSpriteSheet(texture))
+			return;
+		*/
+
+		RetrieveSettings ();
+		TextureImporter textureImporter = (TextureImporter)assetImporter;
+		textureImporter.textureType = TextureImporterType.Sprite;
+		textureImporter.spriteImportMode = SpriteImportMode.Multiple;
+        textureImporter.mipmapEnabled = false;
+		textureImporter.filterMode = FilterMode.Point;
+		textureImporter.spritePixelsPerUnit = m_PixelsPerUnit;
+        textureImporter.textureCompression = TextureImporterCompression.Uncompressed;
+    }
+
+	public void OnPostprocessTexture (Texture2D texture)
+	{
+        string filename = Path.GetFileName(assetPath);
+        if (filename.Length > 4)
+        {
+            if (filename.Substring(0, 4) != "LPC_") return;
+        }
+
+        // Do nothing if it not a LPC Based Sprite
+        if (!IsLpcSpriteSheet (texture))
+			return;
+
+		Debug.Log ("Importing LPC Character Sheet");
+		List<SpriteMetaData> metas = new List<SpriteMetaData>();
+		for (int row = 0; row < m_RowCount; ++row)
+		{
+			for (int col = 0; col < m_ColCount; ++col)
+			{
+				SpriteMetaData meta = new SpriteMetaData();
+				meta.rect = new Rect(col * LPC_SPRITE_SIZE, row * LPC_SPRITE_SIZE, LPC_SPRITE_SIZE, LPC_SPRITE_SIZE);
+                meta.alignment = (int)SpriteAlignment.Custom;
+                meta.pivot = new Vector2(0.5f, 0.0f);
+
+                LpcAnimationState animState = GetAnimationState (row);
+
+				if (!m_ImportEmptySprites) {
+					if ((animState == LpcAnimationState.Hurt && col >= m_HuFrames))
+						break;
+					if ((animState == LpcAnimationState.Shoot && col >= m_ShFrames))
+						break;
+					if ((animState == LpcAnimationState.Slash && col >= m_SlFrames))
+						break;
+					if ((animState == LpcAnimationState.Thrust && col >= m_ThFrames))
+						break;
+					if ((animState == LpcAnimationState.Walk && col >= m_WcFrames))
+						break;
+					if ((animState == LpcAnimationState.Spellcast && col >= m_ScFrames))
+						break;
+				}
+
+				string namePrefix = ResolveLpcNamePrefix (row);
+				meta.name = namePrefix + col;
+				metas.Add(meta);
+			}
+		}
+		TextureImporter textureImporter = (TextureImporter)assetImporter;
+		textureImporter.spritesheet = metas.ToArray();
+    }
+
+	public void OnPostprocessSprites(Texture2D texture, Sprite[] sprites)
+	{
+
+    }
+
+	// Check if a texture is a LPC Spritesheet by
+	// checking the textures width and height
+	private bool IsLpcSpriteSheet(Texture2D texture)
+	{
+        if (texture.width == LPC_SHEET_WIDTH
+            && texture.height == LPC_SHEET_HEIGHT)
+        {
+            return true;
+        }
+
+        return false;
+	}
+
+	public static LpcAnimationState GetAnimationState(int row)
+	{
+		switch (row) {
+		case(0):
+			return LpcAnimationState.Hurt;
+		case(1):
+		case(2):
+		case(3):
+		case(4):
+			return LpcAnimationState.Shoot;
+		case(5):
+		case(6):
+		case(7):
+		case(8):
+			return LpcAnimationState.Slash;
+		case(9):
+		case(10):
+		case(11):
+		case(12):
+			return LpcAnimationState.Walk;
+		case(13):
+		case(14):
+		case(15):
+		case(16):
+			return LpcAnimationState.Thrust;
+		case(17):
+		case(18):
+		case(19):
+		case(20):
+			return LpcAnimationState.Spellcast;
+		default:
+			Debug.LogError ("GetAnimationState unknown row: " + row);
+			return 0;
+		}
+	}
+
+	private string ResolveLpcNamePrefix(int row)
+	{
+		switch (row) {
+		case(0):
+			return "HurtS_";
+		case(1):
+			return "ShootE_";
+		case(2):
+			return "ShootS_";
+		case(3):
+			return "ShootW_";
+		case(4):
+			return "ShootN_";
+		case(5):
+			return "SlashE_";
+		case(6):
+			return "SlashS_";
+		case(7):
+			return "SlashW_";
+		case(8):
+			return "SlashN_";
+		case(9):
+			return "WalkE_";
+		case(10):
+			return "WalkS_";
+		case(11):
+			return "WalkW_";
+		case(12):
+			return "WalkN_";
+		case(13):
+			return "ThrustE_";
+		case(14):
+			return "ThrustS_";
+		case(15):
+			return "ThrustW_";
+		case(16):
+			return "ThrustN_";
+		case(17):
+			return "CastE_";
+		case(18):
+			return "CastS_";
+		case(19):
+			return "CastW_";
+		case(20):
+			return "CastN_";
+		default:
+			Debug.LogError ("ResolveLpcNamePrefix unknown row: " + row);
+			return "";
+		}
+	}
+
+}
