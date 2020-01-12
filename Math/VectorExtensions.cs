2e6745274d634c0111ac006208ba76b627602d5c
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-01-12 20:17:46 +0000

    Added swizzle operations from Vector3 to Vector2

--- Math/VectorExtensions.cs
+++ Math/VectorExtensions.cs
@@ -8,5 +8,35 @@ static public class VectorExtensions
     {
         return new Vector3(inV.x, 0.0f, inV.z);
     }
+
+    public static Vector2 xz(this Vector3 inV)
+    {
+        return new Vector2(inV.x, inV.z);
+    }
+
+    public static Vector2 xy(this Vector3 inV)
+    {
+        return new Vector2(inV.x, inV.y);
+    }
+
+    public static Vector2 yz(this Vector3 inV)
+    {
+        return new Vector2(inV.y, inV.z);
+    }
+
+    public static Vector2 zx(this Vector3 inV)
+    {
+        return new Vector2(inV.z, inV.x);
+    }
+
+    public static Vector2 yx(this Vector3 inV)
+    {
+        return new Vector2(inV.y, inV.x);
+    }
+
+    public static Vector2 zy(this Vector3 inV)
+    {
+        return new Vector2(inV.z, inV.y);
+    }
 };
 
