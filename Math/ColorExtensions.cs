10322f51fe45b1afdcbb47f7f3839e782f2ea63a
Diogo Andrade <diogo.g.andrade@gmail.com>
2020-02-01 18:59:45 +0000

    Added color extensions

---
+++ Math/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static public class ColorExtensions
+{
+    public static Color ChangeAlpha(this Color c, float a)
+    {
+        return new Color(c.r, c.g, c.b, a);
+    }
+};
+
