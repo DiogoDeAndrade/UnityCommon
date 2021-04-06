b3e426c5dae0183a2ed17a76a96f4d4032d80177
Diogo de Andrade <diogo.g.andrade@gmail.com>
2021-04-06 09:54:55 +0100

    Added random from any distribution given by a histogram in a float[]

--- Math/RandomExtension.cs
+++ Math/RandomExtension.cs
@@ -11,4 +11,28 @@ public static class RandomExtension
         return (float)(rnd.NextDouble() * (valMax - valMin) + valMin);
     }
 
+    public static int Range(this System.Random rnd, int valMin, int valMax)
+    {
+        return rnd.Next() % (valMax - valMin) + valMin;
+    }
+
+    public static int Random(this float[] a, System.Random rnd)
+    {
+        float r = (float)rnd.NextDouble();
+        float sum = a.Sum();
+        
+        for (int j = 0; j < a.Length; j++) a[j] /= sum;
+
+        int     i = 0;
+        float   x = 0;
+
+        while (i < a.Length)
+        {
+            x += a[i];
+            if (r <= x) return i;
+            i++;
+        }
+
+        return 0;
+    }
 }
