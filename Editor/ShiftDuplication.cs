664a42a2f48fa66c83aec74d7d99f3c092b9bb82
Diogo de Andrade <diogo.g.andrade@gmail.com>
2024-10-14 16:17:39 +0100

    Added shift-duplicate operation

---
+++ Editor/ShiftDuplication.cs
@@ -0,0 +1,31 @@
+#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+static class ShiftDuplicate
+{
+    private static bool _isDragging;
+
+    static ShiftDuplicate()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    static void OnSceneGUI(SceneView scn)
+    {
+        Event e = Event.current;
+
+        if (!_isDragging && e.modifiers == EventModifiers.Shift && e.type == EventType.MouseDrag)
+        {
+            _isDragging = true;
+            scn.SendEvent(EditorGUIUtility.CommandEvent("Duplicate"));
+        }
+
+        if (e.type is EventType.MouseUp or EventType.Ignore)
+            _isDragging = false;
+    }
+}
+
+#endif
