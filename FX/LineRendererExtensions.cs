29bfd3b07326a17edcf779ea7e1f61dc57b735f8
Diogo de Andrade <diogo.g.andrade@gmail.com>
2025-02-21 18:31:11 +0000

    Added FadeOut tween for line renderer

---
+++ FX/LineRendererExtensions.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public static class LineRendererExtensions
+{
+    public static Tweener.BaseInterpolator FadeOut(this LineRenderer lineRenderer, float time)
+    {
+        return lineRenderer.FadeTo(0.0f, time);
+    }
+
+    public static Tweener.BaseInterpolator FadeTo(this LineRenderer lineRenderer, float targetAlpha, float time)
+    {
+        Color cStart = lineRenderer.startColor;
+        Color cEnd = lineRenderer.endColor;
+
+        if ((cStart.a == targetAlpha) && (cEnd.a == targetAlpha)) return null;
+
+        return lineRenderer.Tween().Interpolate(0.0f, 1.0f, time, (value) =>
+        {
+            lineRenderer.startColor = Color.Lerp(cStart, cStart.ChangeAlpha(targetAlpha), value);
+            lineRenderer.endColor = Color.Lerp(cEnd, cEnd.ChangeAlpha(targetAlpha), value);
+        }, "LRFade");
+    }
+}
