da8c7cbbeb17ab480a8ceac7b99b11006c3b3b19
Diogo de Andrade <diogo.g.andrade@gmail.com>
2025-11-02 02:02:24 +0000

    Added namespace UC to Hover script

--- Animation/Hover.cs
+++ Animation/Hover.cs
@@ -1,29 +1,32 @@
 using UnityEngine;
 
-public class Hover : MonoBehaviour
+namespace UC
 {
-    [SerializeField] private Transform target;
-    [SerializeField] private Vector3   direction = Vector3.up;
-    [SerializeField] private float     offset;
-    [SerializeField] private float     amplitude;
-    [SerializeField] private float     frequency;
-    [SerializeField] private float     baseOffset = 0.0f;
-
-    Vector3 basePos;
-    float   elapsedTime;
-
-    void Start()
+    public class Hover : MonoBehaviour
     {
-        if (target == null) target = this.transform;
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 direction = Vector3.up;
+        [SerializeField] private float offset;
+        [SerializeField] private float amplitude;
+        [SerializeField] private float frequency;
+        [SerializeField] private float baseOffset = 0.0f;
 
-        basePos = target.localPosition;
-    }
+        Vector3 basePos;
+        float elapsedTime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        elapsedTime += Time.deltaTime;
+        void Start()
+        {
+            if (target == null) target = this.transform;
 
-        target.localPosition =  basePos + direction * (offset + Mathf.Sin(elapsedTime * frequency * Mathf.Deg2Rad + baseOffset) * amplitude);
+            basePos = target.localPosition;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            elapsedTime += Time.deltaTime;
+
+            target.localPosition = basePos + direction * (offset + Mathf.Sin(elapsedTime * frequency * Mathf.Deg2Rad + baseOffset) * amplitude);
+        }
     }
 }
