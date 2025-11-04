ccdecd62cc33244560d2a0c5d8e585382a3dc1b4
Diogo de Andrade <diogo.g.andrade@gmail.com>
2025-11-04 11:00:20 +0000

    Added alpha fade extension to SpriteRenderer

--- 2d/SpriteRendererExtensions.cs
+++ 2d/SpriteRendererExtensions.cs
@@ -9,5 +9,9 @@ namespace UC
         {
             return spriteRenderer.Tween().Interpolate(spriteRenderer.color, targetColor, time, (currentValue) => { if (spriteRenderer) spriteRenderer.color = currentValue; }, name);
         }
+        public static Tweener.BaseInterpolator FadeTo(this SpriteRenderer spriteRenderer, float targetAlpha, float time, string name = null)
+        {
+            return spriteRenderer.Tween().Interpolate(spriteRenderer.color, spriteRenderer.color.ChangeAlpha(targetAlpha), time, (currentValue) => { if (spriteRenderer) spriteRenderer.color = currentValue; }, name);
+        }
     }
 }
