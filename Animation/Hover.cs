99d372cb47d25ff7f7cd8a2c8f79edce51970fb9
Diogo de Andrade <diogo.g.andrade@gmail.com>
2026-02-24 13:07:30 +0000

    Added access functions to hover (amplitude/frequency). Also, changing the frequency doesn't jitter the existing animation, it just takes it away from that

--- Animation/Hover.cs
+++ Animation/Hover.cs
@@ -4,13 +4,13 @@ namespace UC
 {
     public class Hover : MonoBehaviour
     {
-        [SerializeField] private Transform target;
-        [SerializeField] private Vector3 direction = Vector3.up;
-        [SerializeField] private float offset;
-        [SerializeField] private float amplitude;
-        [SerializeField] private float frequency;
-        [SerializeField] private float baseOffset = 0.0f;
-        [SerializeField] private bool  useAbs = false;
+        [SerializeField] private Transform  target;
+        [SerializeField] private Vector3    direction = Vector3.up;
+        [SerializeField] private float      offset;
+        [SerializeField] private float      amplitude;
+        [SerializeField] private float      frequency;
+        [SerializeField] private float      baseOffset = 0.0f;
+        [SerializeField] private bool       useAbs = false;
 
         Vector3 basePos;
         float elapsedTime;
@@ -25,11 +25,14 @@ namespace UC
         // Update is called once per frame
         void Update()
         {
-            elapsedTime += Time.deltaTime;
+            elapsedTime += Time.deltaTime * frequency * Mathf.Deg2Rad;
 
-            float wave = Mathf.Sin(elapsedTime * frequency * Mathf.Deg2Rad + baseOffset);
+            float wave = Mathf.Sin(elapsedTime + baseOffset);
             if (useAbs) wave = Mathf.Abs(wave);
             target.localPosition = basePos + direction * (offset + wave * amplitude);
         }
+
+        public void SetFrequency(float v) { frequency = v; }
+        public void SetAmplitude(float v) { amplitude = v; }
     }
 }
