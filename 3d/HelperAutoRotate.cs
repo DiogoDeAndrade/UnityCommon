70b019643203f515e6a13207e1cfe847b2cef5eb
Diogo de Andrade <diogo.g.andrade@gmail.com>
2026-03-28 18:46:11 +0000

    Added option for auto rotate to use original rotation

--- 3d/HelperAutoRotate.cs
+++ 3d/HelperAutoRotate.cs
@@ -4,13 +4,16 @@ public class HelperAutoRotate : MonoBehaviour
 {
     [SerializeField] private float      speed = 360.0f;
     [SerializeField] private Vector3    axis = Vector3.up;
-    [SerializeField] private float startAngle = 0.0f;
+    [SerializeField] private float      startAngle = 0.0f;
+    [SerializeField] private bool       startWithCurrentRotation;
 
-    float angle = 0.0f;
+    float       angle = 0.0f;
+    Quaternion  rotationOffset = Quaternion.identity;
 
     void Start()
     {
         angle = startAngle;
+        if (startWithCurrentRotation) rotationOffset = transform.rotation;
     }
 
     // Update is called once per frame
@@ -20,6 +23,6 @@ public class HelperAutoRotate : MonoBehaviour
         if (angle > 360.0f) angle -= 360.0f;
         else if (angle < 0.0f) angle += 360.0f;
 
-        transform.rotation = Quaternion.AngleAxis(angle, axis);
+        transform.rotation = Quaternion.AngleAxis(angle, axis) * rotationOffset;
     }
 }
