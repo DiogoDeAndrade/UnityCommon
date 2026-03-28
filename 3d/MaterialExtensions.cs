2b11a960dc33cdc4971315ef9cc84ae9de67a4ea
Diogo de Andrade <diogo.g.andrade@gmail.com>
2026-03-28 12:53:26 +0000

    Modified name of MaterialExtension.TweenColor to TweenProperty
    Added 3d distortion shader, that uses color buffer and a fresnel effect to do a shockwave-type effect

--- 3d/MaterialExtensions.cs
+++ 3d/MaterialExtensions.cs
@@ -5,7 +5,19 @@ namespace UC
 
     public static class MaterialExtensions
     {
-        public static Tweener.BaseInterpolator TweenColor(this Material material, GameObject ownerGameObject, string propertyName, Color endColor, float duration)
+        public static Tweener.BaseInterpolator TweenProperty(this Material material, GameObject ownerGameObject, string propertyName, float endValue, float duration)
+        {
+            ownerGameObject.Tween().Stop($"Tween{propertyName}", Tweener.StopBehaviour.SkipToEnd);
+
+            var current = material.GetFloat(propertyName);
+
+            return ownerGameObject.Tween().Interpolate(current, endValue, duration, (value) =>
+            {
+                material.SetFloat(propertyName, value);
+            }, $"Tween{propertyName}").Done(() => material.SetFloat(propertyName, endValue));
+        }
+
+        public static Tweener.BaseInterpolator TweenProperty(this Material material, GameObject ownerGameObject, string propertyName, Color endColor, float duration)
         {
             ownerGameObject.Tween().Stop($"Tween{propertyName}", Tweener.StopBehaviour.SkipToEnd);
 
