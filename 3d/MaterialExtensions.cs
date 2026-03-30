f82bfce5b5fc2b7795559cc3682a7a6ff3b12afd
Diogo de Andrade <diogo.g.andrade@gmail.com>
2026-03-30 20:13:10 +0100

    Fixed issue with binding an animation to a specific material (for multimaterials)

--- 3d/MaterialExtensions.cs
+++ 3d/MaterialExtensions.cs
@@ -7,26 +7,30 @@ namespace UC
     {
         public static Tweener.BaseInterpolator TweenProperty(this Material material, GameObject ownerGameObject, string propertyName, float endValue, float duration)
         {
-            ownerGameObject.Tween().Stop($"Tween{propertyName}", Tweener.StopBehaviour.SkipToEnd);
+            string animName = $"Tween{propertyName}{material.GetInstanceID()}";
+
+            ownerGameObject.Tween().Stop(animName, Tweener.StopBehaviour.SkipToEnd);
 
             var current = material.GetFloat(propertyName);
 
             return ownerGameObject.Tween().Interpolate(current, endValue, duration, (value) =>
             {
                 material.SetFloat(propertyName, value);
-            }, $"Tween{propertyName}").Done(() => material.SetFloat(propertyName, endValue));
+            }, animName).Done(() => material.SetFloat(propertyName, endValue));
         }
 
         public static Tweener.BaseInterpolator TweenProperty(this Material material, GameObject ownerGameObject, string propertyName, Color endColor, float duration)
         {
-            ownerGameObject.Tween().Stop($"Tween{propertyName}", Tweener.StopBehaviour.SkipToEnd);
+            string animName = $"Tween{propertyName}{material.GetInstanceID()}";
+
+            ownerGameObject.Tween().Stop(animName, Tweener.StopBehaviour.SkipToEnd);
 
             var current = material.GetColor(propertyName);
 
             return ownerGameObject.Tween().Interpolate(current, endColor, duration, (value) =>
             {
                 material.SetColor(propertyName, value);
-            }, $"Tween{propertyName}").Done(() => material.SetColor(propertyName, endColor));
+            }, animName).Done(() => material.SetColor(propertyName, endColor));
         }
     }
 }
